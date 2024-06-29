0f570067c7cfd5a6e77e24701bba2cd577cc1bfa
Starlight0798 <244057735@qq.com>
2024-06-29 20:48:32 +0800

    Update TCPConnectScan.cpp

--- src/TCPConnectScan.cpp
+++ src/TCPConnectScan.cpp
@@ -34,3 +34,31 @@ bool TCPConnectScan(const std::string& hostIP, int port) {
         return true;
     }
 }
+
+
+/*
+bool TCPConnectScan(const std::string& hostIP, int port) {
+    int sockfd;
+    struct sockaddr_in sa;
+    
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Error creating socket." << std::endl;
+        return false;
+    }
+
+    memset(&sa, 0, sizeof(struct sockaddr_in));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    sa.sin_addr.s_addr = inet_addr(hostIP.c_str());
+
+    if (connect(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+        std::cerr << "Port " << port << " closed." << std::endl;
+        close(sockfd);
+        return false;
+    } else {
+        std::cout << "Port " << port << " open." << std::endl;
+        close(sockfd);
+        return true;
+    }
+}
+*/
